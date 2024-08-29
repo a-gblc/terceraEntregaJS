7b5a11a4fe5f0667578eb19f3c6461b789bbdefb
agarciabecona <a.garciabecona@gmail.com>
2024-08-28 21:07:14 -0300

    tercera entrega

--- js/funciones.js
+++ js/funciones.js
@@ -1,53 +1,70 @@
-const tasasMensuales = [0.008, 0.015, 0.025, 0.035];
-const tiposPrestamos = ['hipotecario', 'pyme', 'personal', 'automotor'];
+const prestamos = [
+    { id: 1, tipo: "Hipotecario", tasa: 0.005 },
+    { id: 2, tipo: "Pyme", tasa: 0.015 },
+    { id: 3, tipo: "Personal", tasa: 0.025 },
+    { id: 4, tipo: "Automotor", tasa: 0.035 },
+];
 
-class Prestamo {
-    constructor(tipo, plazo, monto) {
-        this.tipo = tipo;
-        this.plazo = plazo;
-        this.monto = monto;
-        this.tasa = this.tasaInteres();
-    }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+localStorage.clear();
+
+    const tipoPrestamoDesplegable = document.getElementById('tipoPrestamoDesplegable');
+    prestamos.forEach(prestamo => {
+        const eleccionPrestamo = document.createElement('option');
+        eleccionPrestamo.value = prestamo.tipo;
+        eleccionPrestamo.text = prestamo.tipo;
+        tipoPrestamoDesplegable.appendChild(eleccionPrestamo);
+    });
+
+
+});
+
+
+function obtenerTasa(tipo) {
+    return prestamos.find(p => p.tipo === tipo).tasa;
+}
+
+
+const calcularCuotaMensual = (tipo, plazo, monto) =>
+((monto * obtenerTasa(tipo)) / (1 - Math.pow(1 + obtenerTasa(tipo), -plazo))).toFixed(2);
+
+
+function calcularPrestamo() {
+
+const tipo = document.getElementById('tipoPrestamoDesplegable').value;
+const monto = Math.abs(parseFloat(document.getElementById('monto').value));
+const plazo = Math.abs(parseInt(document.getElementById('plazo').value));
+const tasa = obtenerTasa(tipo);
+
+const cuotaMensual = calcularCuotaMensual(tipo, plazo, monto, tasa);
+
+const prestamoSeleccionado = { tipo, plazo, monto, cuotaMensual, tasa};
+
+    localStorage.setItem('prestamoSeleccionado', JSON.stringify(prestamoSeleccionado));
+    mostrarResumen();
+
+}
+
+function mostrarResumen() {
+    const prestamoSeleccionado = JSON.parse(localStorage.getItem('prestamoSeleccionado'));
+    const resultadoElement = document.getElementById('simulacion');
+
+
+    if (prestamoSeleccionado) {
+        resultadoElement.innerHTML = `
+            <div class="resultadoElement">
+            <h3>Resumen de la simulaci&oacuten:</h3>
+            <p>Tipo de Préstamo: ${prestamoSeleccionado.tipo}</p>
+            <p>Monto: ${prestamoSeleccionado.monto} pesos</p>
+            <p>Plazo: ${prestamoSeleccionado.plazo} meses</p>
+            <p>Tasa aplicada: ${prestamoSeleccionado.tasa* 100} % mensual</p>
+            <p>Cuota Mensual: ${prestamoSeleccionado.cuotaMensual} pesos</p>
+            </div>`;
     
-    tasaInteres() {
-        
-        let indiceTiposPrestamo = tiposPrestamos.indexOf(this.tipo);
-        return tasasMensuales[indiceTiposPrestamo];
-    }
-    
-    calcularPrestamo() {
-        let cuotaMensual = this.monto / this.plazo;
-        let cuotaTotal = cuotaMensual + (cuotaMensual * this.tasa);
-        console.log('El valor de su cuota mensual es de ' + cuotaTotal + ' pesos.');
-    }
+    } 
 }
-
-let eleccionPrestamo = prompt('Ingrese el préstamo que desea simular (hipotecario, pyme, personal, automotor):').toLowerCase();
-while (!tiposPrestamos.includes(eleccionPrestamo)) {
-    console.log('El tipo de préstamo no es válido. Elegí entre : hipotecario, pyme, personal, automotor.');
-    eleccionPrestamo = prompt('Ingrese el préstamo que desea simular (hipotecario, pyme, personal, automotor):').toLowerCase();
-    
-}
-
-console.log('Ha elegido un préstamo :' + '' + ' ' + eleccionPrestamo );
-
-let plazoDelPrestamoMeses = parseInt(prompt('Ingrese el plazo en meses:'));
-while (isNaN(plazoDelPrestamoMeses)) {
-    console.log('El valor ingresado no es válido.');
-    plazoDelPrestamoMeses = parseInt(prompt('Ingrese el plazo en meses'));
-}
-
-let montoSolicitado = parseInt(prompt('Ingrese el monto solicitado'));
-while (isNaN(montoSolicitado)) {
-    console.log('El valor ingresado no es válido.');
-    montoSolicitado = parseInt(prompt('Ingrese el monto solicitado'));
-}
-
-
-const prestamo1 = new Prestamo(eleccionPrestamo, plazoDelPrestamoMeses, montoSolicitado);
-prestamo1.calcularPrestamo();
-console.log(prestamo1);
-
-
 
 
---
+++ js/navegacion.js
@@ -0,0 +1,51 @@
+
+
+//La creacion de la barra de navegacion desde .js esta hecho solamente para index.html, en el resto de las paginas no lo 
+//termine. Cuando voy al INICIO desde la barra de navegacion me da un error, evidentemente no esta bien hecho el tema de crearlo desde el .js
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.querySelector('.header');
+
+    const navegacion = document.createElement('div');
+    const nav = document.createElement('nav');
+    const ul = document.createElement('ul');
+
+    const enlaces = [
+
+        { link: "index", 
+        nombre: "Inicio" },
+
+        { link: "hipotecario", 
+        nombre: "Hipotecario" },
+
+        { link: "automotor", 
+        nombre: "Automotor" },
+
+        { link: "pyme", 
+        nombre: "Pyme" },
+
+        { link: "prestamoPersonal", 
+        nombre: "Préstamo Personal" }
+    ];
+
+    header.appendChild(navegacion);
+    navegacion.appendChild(nav);
+    nav.appendChild(ul);
+    navegacion.className = "navbar";
+    nav.className = "nav"; 
+    for (const link of enlaces) {
+        const li = document.createElement('li');
+        li.innerHTML = `<a href="./pages/${link.link}.html">${link.nombre}</a>`;
+        ul.appendChild(li);
+    }
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const anioPiePag = document.getElementById('pieDePagina');
+    const anioActual = new Date().getFullYear();
+    if (anioPiePag) {
+        anioPiePag.textContent = `CoderHouse ${anioActual}`;
+    }
+});
